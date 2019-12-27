3263127f51728ba0f325ff3e300a151498f21255
vjgarciag96 <vjgarciaw@gmail.com>
2019-12-27 17:13:14 +0100

    update latest rates every second

--- rates/src/main/java/com/vjgarcia/rates/domain/RatesUseCases.kt
+++ rates/src/main/java/com/vjgarcia/rates/domain/RatesUseCases.kt
@@ -2,19 +2,19 @@ package com.vjgarcia.rates.domain
 
 import arrow.core.Either
 import com.vjgarcia.rates.data.repository.RatesRepository
-import io.reactivex.Single
+import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 internal class GetLatestRates(
     private val ratesRepository: RatesRepository
 ) {
 
-    operator fun invoke(): Single<Either<LatestRatesError, LatestRates>> =
-        Single.create<Either<LatestRatesError, LatestRates>> { emitter ->
-            val latestRatesResult = ratesRepository.latestRates()
-                .mapLeft { LatestRatesError }
-                .map { it.toDomain() }
-
-            emitter.onSuccess(latestRatesResult)
-        }.subscribeOn(Schedulers.io())
+    operator fun invoke(): Observable<Either<LatestRatesError, LatestRates>> =
+        Observable.interval(1L, TimeUnit.SECONDS)
+            .map {
+                ratesRepository.latestRates()
+                    .mapLeft { LatestRatesError }
+                    .map { it.toDomain() }
+            }.subscribeOn(Schedulers.io())
 }
